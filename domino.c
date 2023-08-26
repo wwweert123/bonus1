06aee7e0ef4ce4fcbab64398d6cb9e974a0a7c1b
wwweert123 <puachengxuan.fp@gmail.com>
2023-08-26 22:55:21 +0800

    add skeleton

---
+++ domino.c
@@ -0,0 +1,14 @@
+/**
+ * CS1010 Semester 1 AY23/24
+ * Bonus question By Frederick
+ *
+ * @file: bmi.c
+ * @author: XXX (Group YYY)
+ */
+
+#include "cs1010.h"
+
+int main()
+{
+    // TODO: find maximal number of dominoes, which can be placed
+}
